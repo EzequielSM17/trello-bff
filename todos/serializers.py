ff239753a9faa5283bbbfab48616f6473ccc4f45
EzequielSM17 <ezequielsolermartinez@gmail.com>
2024-08-08 23:47:56 +0200

    Nuevo serializer para end point "todos_specific"

--- todos/serializers.py
+++ todos/serializers.py
@@ -3,19 +3,6 @@ from users.models import User
 from todos.models import ToDoTypes, ToDo, ToDoState
 
 
-class ToDoSerializer(serializers.ModelSerializer):
-    people = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=User.objects.all()
-    )
-
-    class Meta:
-        model = ToDo
-        fields = ('id', 'title', 'description',
-                  'date', 'deadline', 'people', 'type', 'state')
-        read_only_Fields = ('id',)
-
-
 class ToDoTypesSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -29,4 +16,31 @@ class ToDoStateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDoState
         fields = ('id', 'name')
+        read_only_Fields = ('id',)
+
+
+class ToDoSerializer(serializers.ModelSerializer):
+    people = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = ToDo
+        fields = ('id', 'title', 'description',
+                  'date', 'deadline', 'people', 'type', 'state')
+        read_only_Fields = ('id',)
+
+
+class ToDoWithTypeSerializer(serializers.ModelSerializer):
+    people = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all()
+    )
+    type = ToDoTypesSerializer()
+
+    class Meta:
+        model = ToDo
+        fields = ('id', 'title', 'description',
+                  'date', 'deadline', 'people', 'type', 'state')
         read_only_Fields = ('id',)
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
-from django.shortcuts import get_object_or_404
+from rest_framework import status
 from todos.models import ToDo, ToDoTypes, ToDoState
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from todos.serializers import ToDoSerializer, ToDoTypesSerializer, ToDoStateSerializer
+from todos.serializers import ToDoSerializer, ToDoTypesSerializer, ToDoStateSerializer, ToDoWithTypeSerializer
 from rest_framework.response import Response
 # Create your views here.
 
@@ -15,11 +15,25 @@ class ToDoViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(people=self.request.user)
 
+    def create(self, request):
+        try:
+            user = self.request.user
+            data = {'people': [user.id]}
+            data.update(request.data)
+            todo_serializer = self.serializer_class(data=data)
+            if todo_serializer.is_valid():
+                todo_serializer.save()
+                return Response({'company': todo_serializer.data}, status=status.HTTP_201_CREATED)
+            return Response(todo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": "Error al crear una tarea", "details": str(e)},
+                            status=status.HTTP_400_BAD_REQUEST)
+
 
 class ToDoSpecificStateView(viewsets.ViewSet):
     queryset = ToDo.objects.all()
     permission_classes = [IsAuthenticated]
-    serializer_class = ToDoSerializer
+    serializer_class = ToDoWithTypeSerializer
 
     def get_queryset(self):
         return self.queryset.filter(people=self.request.user)
@@ -28,7 +42,7 @@ class ToDoSpecificStateView(viewsets.ViewSet):
         queryset = ToDo.objects.all()
         todos = queryset.filter(
             state=pk, people=self.request.user)
-        serializer = ToDoSerializer(todos, many=True)
+        serializer = ToDoWithTypeSerializer(todos, many=True)
         return Response(serializer.data)
 
 
